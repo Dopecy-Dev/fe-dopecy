80811374b50a06fb7b0621b813cbdf56210445f2
nouman <noumanbwn@gmail.com>
2024-07-20 07:20:54 +0500

    -- typo fixes

--- src/shared/TopBar/TopBar.jsx
+++ src/shared/TopBar/TopBar.jsx
@@ -48,7 +48,7 @@ function TopBar() {
                         color: 'text.cstmwhite',
                         ml: 1.5
                     }}>
-                        Bird Coupon
+                        Early Bird Deals
                     </Typography>
                 </Box>
                 {/* </Link> */}
